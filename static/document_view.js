b0c82688650220fccfe4f479eb54bc319a760694
rushkock <ruchella_kock@hotmail.com>
2020-10-08 21:32:15 +0200

    pdf added, search on keyup, common js codes in main, new data file

--- static/document_view.js
+++ static/document_view.js
@@ -1,33 +1,30 @@
 window.onload =  function(){
    var requests = [d3.csv('data/data_r.csv')]
    Promise.all(requests).then(function(response) {
+     // read data again
      var data = lower(response);
+
+     // get the field parameter from url
+     // in this case it is the title
      const queryString = window.location.search;
      const urlParams = new URLSearchParams(queryString);
-     console.log(queryString)
      const title = urlParams.get('title');
-     console.log(title)
-     data_document= data[0].filter(element => element.title.includes(title));
-     console.log(data_document)
+     // search in data for title
+     let data_document= data[0].filter(element => element.title.includes(title));
+     // preprocess data for pretty printing
+     data_document = preprocess(data_document);
+     // add data to html page
      seperate_view(data_document[0]);
   }).catch(function(e) {
       throw(e);
   });
 };
+
+
 function seperate_view(data_document) {
+  // mustache template
+  // dynamically fill template of documnent view.html
   var template = document.getElementById('seperate_view_template').innerHTML;
   var rendered = Mustache.render(template, data_document);
   document.getElementById('fill_seperate_view_template').innerHTML = rendered;
-}
-
-function lower(obj){
-  for (var prop in obj) {
-    if (typeof obj[prop] === 'string') {
-      obj[prop] = obj[prop].toLowerCase();
-    }
-    if (typeof obj[prop] === 'object') {
-      lower(obj[prop]);
-      }
-    }
-  return obj;
 }
--- static/search.js
+++ static/search.js
@@ -3,43 +3,37 @@ window.onload =  function(){
    Promise.all(requests).then(function(response) {
      data = lower(response);
      console.log(data)
+     // onkeyup of search bar start searching
+     $("#search_input").keyup(search)
   }).catch(function(e) {
       throw(e);
   });
 };
 
 function search(){
+  // search function for the search bar
   const input = document.getElementById('search_input').value.toLowerCase();
-  console.log(input)
-  const found = data[0].filter(element => element.title.includes(input) || element.courses.includes(input) || element.authors.includes(input) || element.topic1.includes(input) || element.topic2.includes(input) || element.topic3.includes(input) || element.keywords.includes(input));
-  console.log(found)
+  let found = data[0].filter(element => element.title.includes(input) || element.courses.includes(input) || element.authors.includes(input) || element.topic1.includes(input) || element.topic2.includes(input) || element.topic3.includes(input) || element.keywords.includes(input));
+  found = preprocess(found);
   add_all_results(found)
 }
 
-function lower(obj){
-  for (var prop in obj) {
-    if (typeof obj[prop] === 'string') {
-      obj[prop] = obj[prop].toLowerCase();
-    }
-    if (typeof obj[prop] === 'object') {
-      lower(obj[prop]);
-      }
-    }
-  return obj;
-}
-
 function update_files(data_document, target_id) {
+  // mustache template
+  // this function will fill in the information of one document
   var template = document.getElementById('mp_template').innerHTML;
   var rendered = Mustache.render(template, data_document);
   document.getElementById(target_id).innerHTML = rendered;
 }
 
 function add_all_results(found){
+  // this function adds all the document together on the page
+  // remove old document
   d3.selectAll(".result_document").remove();
   for (i in found){
-    console.log(found[i])
+    // id for each document
     const target = "target" + i;
     d3.select(".results").append("div").attr("id", target).attr("class", "result_document");
-    update_files(found[i], target)
+    update_files(found[i], target);
   }
 }
