e3f8f26024e114460b6ce41c8bef17dbbb505a1b
Babette Mooij <babettemooij07@gmail.com>
2020-10-09 21:08:35 +0200

    added total dataset, add favicon to documentview, started filterfile

--- static/main.js
+++ static/main.js
@@ -1,41 +1,37 @@
-function myFunction() {
-  document.getElementById('demo').innerHTML = 'Paragraph changed.';
-}
-
-function preprocess(obj){
+function preprocess(obj) {
   for (var prop in obj) {
     // create new property processed title with the first letter of each word in title as capital letter
-    const title = obj[prop]["title"];
-    if (title){
-      obj[prop]["processed_title"] = titleCase(title);
-    };
-    // add abstarct only if the document is a paper if its a slide set field to empty
-    const type = obj[prop]["type"];
-    if (type === "paper"){
-      obj[prop]["show_abstract"] = preprocess_abstract_text(obj[prop]["abstract"]);
+    const title = obj[prop]['title'];
+    if (title) {
+      obj[prop]['processed_title'] = titleCase(title);
     }
-    else if (type === "slides"){
-      obj[prop]["show_abstract"] = "";
+    // add abstract only if the document is a paper if its a slide set field to empty
+    const type = obj[prop]['type'];
+    if (type === 'paper') {
+      obj[prop]['show_abstract'] = preprocess_abstract_text(
+        obj[prop]['abstract']
+      );
+    } else if (type === 'slides') {
+      obj[prop]['show_abstract'] = '';
     }
-    // add abstarct only if the document is a paper if its a slide set field to empty
-    const keywords = obj[prop]["keywords"];
-    if (keywords){
-      obj[prop]["show_keywords"] = obj[prop]["keywords"];
+    // add abstract only if the document is a paper if its a slide set field to empty
+    const keywords = obj[prop]['keywords'];
+    if (keywords) {
+      obj[prop]['show_keywords'] = obj[prop]['keywords'];
+    } else if (!keywords) {
+      obj[prop]['show_keywords'] = 'None';
     }
-    else if (!keywords){
-      obj[prop]["show_keywords"] = "None";
-    }
-    }
+  }
   return obj;
 }
 
-function preprocess_abstract_text(str){
-  let short_abstract = str.split(' ').slice(0,50).join(' ');
-  short_abstract = "Abstract: " + short_abstract + " (... see more)";
-  return short_abstract
+function preprocess_abstract_text(str) {
+  let short_abstract = str.split(' ').slice(0, 50).join(' ');
+  short_abstract = 'Abstract: ' + short_abstract + ' (... see more)';
+  return short_abstract;
 }
 
-function lower(obj){
+function lower(obj) {
   // set everything to lowercase except filenames
   for (var prop in obj) {
     if (typeof obj[prop] === 'string' && prop != 'filename') {
@@ -43,19 +39,20 @@ function lower(obj){
     }
     if (typeof obj[prop] === 'object' && prop != 'filename') {
       lower(obj[prop]);
-      }
     }
+  }
   return obj;
 }
 
 function titleCase(str) {
-    // set first letter of each word in str to capital letter
-   var splitStr = str.toLowerCase().split(' ');
-   for (var i = 0; i < splitStr.length; i++) {
-       // You do not need to check if i is larger than splitStr length, as your for does that for you
-       // Assign it back to the array
-       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-   }
-   // Directly return the joined string
-   return splitStr.join(' ');
+  // set first letter of each word in str to capital letter
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(' ');
 }
--- static/search.js
+++ static/search.js
@@ -1,21 +1,32 @@
-window.onload =  function(){
-   var requests = [d3.csv('data/data_r.csv')]
-   Promise.all(requests).then(function(response) {
-     data = lower(response);
-     console.log(data)
-     // onkeyup of search bar start searching
-     $("#search_input").keyup(search)
-  }).catch(function(e) {
-      throw(e);
-  });
+window.onload = function () {
+  var requests = [d3.csv('data.csv')];
+  Promise.all(requests)
+    .then(function (response) {
+      data = lower(response);
+      console.log(data);
+      // onkeyup of search bar start searching
+      $('#search_input').keyup(search);
+    })
+    .catch(function (e) {
+      throw e;
+    });
 };
 
-function search(){
+function search() {
   // search function for the search bar
   const input = document.getElementById('search_input').value.toLowerCase();
-  let found = data[0].filter(element => element.title.includes(input) || element.courses.includes(input) || element.authors.includes(input) || element.topic1.includes(input) || element.topic2.includes(input) || element.topic3.includes(input) || element.keywords.includes(input));
+  let found = data[0].filter(
+    (element) =>
+      element.title.includes(input) ||
+      element.courses.includes(input) ||
+      element.authors.includes(input) ||
+      element.topic1.includes(input) ||
+      element.topic2.includes(input) ||
+      element.topic3.includes(input) ||
+      element.keywords.includes(input)
+  );
   found = preprocess(found);
-  add_all_results(found)
+  add_all_results(found);
 }
 
 function update_files(data_document, target_id) {
@@ -26,14 +37,17 @@ function update_files(data_document, target_id) {
   document.getElementById(target_id).innerHTML = rendered;
 }
 
-function add_all_results(found){
+function add_all_results(found) {
   // this function adds all the document together on the page
   // remove old document
-  d3.selectAll(".result_document").remove();
-  for (i in found){
+  d3.selectAll('.result_document').remove();
+  for (i in found) {
     // id for each document
-    const target = "target" + i;
-    d3.select(".results").append("div").attr("id", target).attr("class", "result_document");
+    const target = 'target' + i;
+    d3.select('.results')
+      .append('div')
+      .attr('id', target)
+      .attr('class', 'result_document');
     update_files(found[i], target);
   }
 }
